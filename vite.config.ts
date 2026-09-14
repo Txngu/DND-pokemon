2acfed96a3d38cd31a45ca7f5ae8c266976330b7
Txngu <137008663+Txngu@users.noreply.github.com>
2026-09-14 14:04:01 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/pokemon-phone/",
+  base: "/DND-pokemon/",
   plugins: [react()],
   resolve: {
     alias: {
