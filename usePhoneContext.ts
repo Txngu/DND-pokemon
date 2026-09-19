94f0ee23c892c2024571ae5f6920f615e109d102
Txngu <137008663+Txngu@users.noreply.github.com>
2026-09-19 23:02:13 +0200

    Delete Wrong File upload

--- usePhoneContext.ts
+++
@@ -1,6 +0,0 @@
-import { useOutletContext } from "react-router-dom";
-import type { Profile } from "@/types/database.types";
-
-export function usePhoneContext() {
-  return useOutletContext<{ profile: Profile }>();
-}
