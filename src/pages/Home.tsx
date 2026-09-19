37fb8ffeceac3b616c3ecfd20f964bde0102e61f
Txngu <137008663+Txngu@users.noreply.github.com>
2026-09-19 22:53:09 +0200

    Delete src directory

--- src/pages/Home.tsx
+++
@@ -1,7 +0,0 @@
-import { HomeScreen } from "@/components/phone/HomeScreen";
-import { usePhoneContext } from "@/hooks/usePhoneContext";
-
-export default function Home() {
-  const { profile } = usePhoneContext();
-  return <HomeScreen profile={profile} />;
-}
